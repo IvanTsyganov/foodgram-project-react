6ba4dd4bdf801535a7a2ad4231ec995421db02b9
Иван Цыганов <zamegagurrendan@gmail.com>
2023-05-25 23:37:06 +0500

    v3  fixed flake8 mistakes2

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,15 +2,15 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from foodgram.settings import FILE_NAME
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             ShoppingCart, Tag)
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-
 from users.models import Follow, UserFoodgram
-from foodgram.settings import FILE_NAME
+
 from .filters import RecipeFilter
 from .methods import favorite_shopping_cart
 from .pagination import CustomPaginator
